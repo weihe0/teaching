600dd6ed437d6f1da8c6eea038fc1fe1022185b6
Wei He <weihe86@outlook.com>
2023-03-08 00:41:06 +0800

    Add both AI and C language courses

---
+++ src/components/AI.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="slide">
+    <h1>项目一 智能机器人</h1>
+    <ul>
+      <li>智能机器人的基本含义</li>
+      <li>典型的智能机器人</li>
+      <li>主要应用领域</li>
+      <li>关键技术</li>
+    </ul>
+  </div>
+  <div class="slide">
+    <h1>智能机器人的基本含义</h1>
+    <ul>
+      <li>智能机器人的基本含义</li>
+      <li>典型的智能机器人</li>
+      <li>主要应用领域</li>
+      <li>关键技术</li>
+    </ul>
+  </div>
+  <div class="slide">
+    <h1>智能机器人的基本含义</h1>
+    <ul>
+      <li>感觉要素</li>
+      <li>运动要素</li>
+      <li>思考要素</li>
+    </ul>
+  </div>
+  <div class="slide">
+    <h1>常见类型</h1>
+    <ul>
+      <li>工业智能机器人</li>
+      <li>农业智能机器人</li>
+      <li>服务智能机器人</li>
+      <li>探索智能机器人</li>
+    </ul>
+  </div>
+  <div class="slide">
+    <h1>关键技术</h1>
+    <ul>
+      <li>多传感器信息融合</li>
+      <li>导航与定位</li>
+      <li>路径规划</li>
+      <li>智能控制</li>
+      <li>人机接口技术</li>
+    </ul>
+  </div>
+  <div class="slide">
+    <h1>智能机器人的发展方向</h1>
+    <ul>
+      <li>可编程机器人：编程序完成重复操作</li>
+      <li>感知机器人：具有感知周围环境的能力</li>
+      <li>智能机器人：具有识别、推理、规划和学习等机制</li>
+    </ul>
+  </div>
+  <div class="slide">
+    <h1>技术方向</h1>
+    <ul>
+      <li>机器人网络化</li>
+      <li>软计算方法：概率推理、神经网络、遗传算法</li>
+      <li>机器学习</li>
+      <li>智能人机接口</li>
+    </ul>
+  </div>
+  <div class="slide">
+    <h1>智能制造案例</h1>
+    <a href="https://www.koloe.cn/information/17330.html" target="_blank">
+      用特斯拉超级工厂经验，打造新一代制造业生产模式
+    </a>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+a {
+  max-width: 80%;
+  font-family: sans-serif;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import HelloWorld from "./components/HelloWorld.vue";
+import AI from './components/AI.vue'
+import CLang from "./components/CLang.vue";
+const app=createApp(App);
+const routes = [
+    { path: '/', component: CLang, },
+    { path: '/ai', component: AI, },
+]
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+app.use(router);
+app.mount('#app');
