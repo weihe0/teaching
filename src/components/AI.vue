c996e2c4e08b39791001a26875f050cb48c92390
Wei He <weihe86@outlook.com>
2023-03-14 23:05:23 +0800

    AI: big data

--- src/components/AI.vue
+++ src/components/AI.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="slide">
-    <h1>项目一 智能机器人</h1>
+    <h1>项目二 智能机器人</h1>
     <ul>
       <li>智能机器人的基本含义</li>
       <li>典型的智能机器人</li>
@@ -67,6 +67,61 @@
       用特斯拉超级工厂经验，打造新一代制造业生产模式
     </a>
   </div>
+  <div class="slide">
+    <h1>大数据</h1>
+    <ul>
+      <li>理解大数据</li>
+      <li>大数据的应用领域</li>
+      <li>大数据技术应用的壁垒</li>
+    </ul>
+  </div>
+  <div class="slide">
+    <h1>大数据的特征</h1>
+    <ul>
+      <li>Volume：容量大</li>
+      <li>Variety：种类多</li>
+      <li>Velocity：速度快</li>
+      <li>Veracity：真实度高</li>
+    </ul>
+  </div>
+  <div class="slide">
+    <h1>数据科学与大数据</h1>
+    <ul>
+      <li>数据科学是一门基于数据处理的科学</li>
+      <li>互联网巨头驱动</li>
+      <li><img src="../assets/meta-logo.png" class="inline" alt="Meta Logo" />Meta（原Facebook）
+        <img src="../assets/google-log.webp" class="inline" alt="Google Logo" />oogle
+      <img src="../assets/alibaba-logo.jpg" class="inline" alt="Alibaba Logo">阿里巴巴集团</li>
+      <li>理论部分：统计学、机器学习、数据可视化</li>
+      <li>应用部分：大数据加工、大数据计算、大数据管理</li>
+    </ul>
+  </div>
+  <div class="slide">
+    <h1>大数据的方向</h1>
+    <ul>
+      <li>数据平台：稳定、安全的大数据平台</li>
+      <li>数据采集：从网页、数据库、传感器获取数据</li>
+      <li>数据仓库：实现海量数据的存储</li>
+      <li>数据处理：清洗或处理数据</li>
+      <li>数据分析、数据挖掘、机器学习：从大数据中发现有用的信息</li>
+    </ul>
+  </div>
+  <div class="slide">
+    <h1>大数据处理基本流程</h1>
+    <ol>
+      <li>数据采集</li>
+      <li>数据清洗，<em>最重要的阶段</em></li>
+      <li>数据存储</li>
+      <li>数据处理</li>
+    </ol>
+  </div>
+  <div class="slide">
+    <h1>大数据技术应用的壁垒</h1>
+    <ul>
+      <li>成本壁垒：手机或购买数据的成本高，处理流程复杂，存储与计算成本高</li>
+      <li>信息交流壁垒：政府、企业之间存在信息壁垒</li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -76,5 +131,9 @@
 a {
   max-width: 80%;
   font-family: sans-serif;
+}
+.inline {
+  display: inline;
+  height: 1em;
 }
 </style>
