624929ec12016f1c83e9429fc1067ff65ba91894
Wei He <weihe86@outlook.com>
2023-03-23 13:11:31 +0800

    add more animations

--- src/components/Electrical.vue
+++ src/components/Electrical.vue
@@ -28,10 +28,20 @@
     <p>\(z(t)=R_x\cos t + i R_x\sin t\)</p>
   </div>
   <div class="slide">
+    <h1>曲线的参数形式2</h1>
+    <canvas width="960" height="540" ref="lemniscate"></canvas>
+    <p>\[z(t)=\frac{a\cos t}{1+\sin^2 t}+i\frac{a\sin t\cos t}{1+\sin^2 t}\]</p>
+  </div>
+  <div class="slide">
+    <h1>直线也是曲线</h1>
+    <canvas width="960" height="540" ref="straight"></canvas>
+    <p>\[z(t)=2t+i(t+1)\]</p>
+  </div>
+  <div class="slide">
     <h1>积分的参数形式</h1>
     <p>\[\int_Cf(z(t))\mathrm{d}z(t)=\int_0^tf(z)\frac{\mathrm{d}z}{\mathrm{d}t}\mathrm{d}t
       =\int_0^tf(z)z'(t)\mathrm{d}t\]</p>
-    <p>\[(R_x\cos t+i R_y\sin t)'=-R_x\sin t+i R_y \cos t\]</p>
+    <p>例如\[(R_x\cos t+i R_y\sin t)'=-R_x\sin t+i R_y \cos t\]</p>
   </div>
   <div class="slide">
     <h1>柯西——古萨定理</h1>
@@ -42,10 +52,27 @@
     </ul>
   </div>
   <div class="slide">
+    <h1>柯西积分</h1>
+    <ul>
+      <li>\(\frac{f(z)}{z-z_0}\)在\(z=z_0\)处不连续</li>
+      <li>但可以在\(z=z_0\)的<em>邻域</em>内对<em>闭合曲线</em>做积分</li>
+      <li>\[f(z_0)=\frac{1}{2\pi i}\oint\frac{f(z)}{z-z_0}\mathrm{d}z\]</li>
+      <li>\[\frac{1}{2\pi i}\oint\frac{\sin z}{z}\mathrm{d}z=\frac{1}{2\pi i}\oint\frac{\sin z}{z-0}\mathrm{d}z=\sin 0=0\]</li>
+    </ul>
+  </div>
+  <div class="slide">
     <h1>原函数</h1>
     <ul>
       <li>\(F'(z)=f(z)\)，\(F(z)\)是\(f(z)\)的<em>原函数</em></li>
       <li>\[\int_{z_0}^{z_1} f(z)\mathrm{d}z=F(z_1)-F(z_0)\]</li>
+    </ul>
+  </div>
+  <div class="slide">
+    <h1>解析函数的高阶导数</h1>
+    <ul>
+      <li>利用柯西积分计算导数</li>
+      <li>证明方法：将\[f'(z_0)=\lim_{\Delta z\to 0}\frac{f(z_0+\Delta z)-f(z_0)}{\Delta z}\]转化为两个<em>闭曲线积分</em>的<em>差</em></li>
+      <li>高阶导数用法：积分&rightarrow;导数</li>
     </ul>
   </div>
 </template>
@@ -55,8 +82,12 @@ import {onMounted, ref} from "vue";
 
 const graph = ref<HTMLCanvasElement | null>(null);
 const curve = ref<HTMLCanvasElement | null>(null);
+const lemniscate=ref<HTMLCanvasElement | null>(null);
+const straight=ref<HTMLCanvasElement | null>(null);
 let c: CanvasRenderingContext2D;
 let c2: CanvasRenderingContext2D;
+let c3: CanvasRenderingContext2D;
+let c4: CanvasRenderingContext2D;
 const real=ref(2);
 const imaginary=ref(3);
 onMounted(() => {
@@ -70,6 +101,18 @@ onMounted(() => {
   const anime_curve=animation_curve();
   setInterval(()=>{
     anime_curve.next();
+  }, 1000/60);
+
+  c3=lemniscate.value!.getContext('2d')!;
+  const anime_lemniscate=animation_lemniscate();
+  setInterval(()=>{
+    anime_lemniscate.next();
+  }, 1000/60);
+
+  c4=straight.value!.getContext('2d')!;
+  const anime_straight=animation_straight();
+  setInterval(()=>{
+    anime_straight.next();
   }, 1000/60);
 })
 
@@ -86,7 +129,7 @@ function* animation_curve(){
       c2.font="1em sans-serif"
       c2.save();
       c2.setTransform(1,0,0,1,c2.canvas.width/2,c2.canvas.height/2);
-      c2.fillText(`t=${i+1}π/360`, -c2.measureText(`t=${i+1}π/360`).width/2, 0);
+      c2.fillText(`t=${i+1}π/180`, -c2.measureText(`t=${i+1}π/180`).width/2, 0);
       c2.restore();
       c2.beginPath();
       c2.ellipse(0,0,300,240,0, 0, angle);
@@ -108,8 +151,63 @@ function* animate_number(){
       yield ;
     }
   }
+}
 
+function* animation_lemniscate(){
+  const a=300;
+  c3.transform(1,0,0,-1,c3.canvas.width/2,c3.canvas.height/2);
+  c3.strokeStyle="SpringGreen";
+  while(true){
+    let t=0;
+    let s=Math.PI/180;
+    let [cos, sin]=[Math.cos(t),Math.sin(t)];
+    let [x0,y0]=[a*cos/(1+sin*sin),a*sin*cos/(1+sin*sin)];
+    c3.beginPath();
+    c3.lineWidth=3;
+    c3.moveTo(x0,y0);
+    for(let i=1;i<=360;i++){
+      c3.clearRect(-c3.canvas.width/2,-c3.canvas.height/2, c3.canvas.width,c3.canvas.height);
+      t += s;
+      [cos, sin]=[Math.cos(t),Math.sin(t)];
+      let d=1+sin*sin;
+      let [x1,y1]=[a*cos/d,a*sin*cos/d];
+      c3.lineTo(x1,y1);
+      c3.stroke();
+      c3.fillStyle="Green";
+      c3.font="1em sans-serif"
+      c3.save();
+      c3.setTransform(1,0,0,1,c3.canvas.width/2,c3.canvas.height/2);
+      c3.fillText(`t=${i+1}π/180`, -c3.measureText(`t=${i+1}π/180`).width/2, 200);
+      c3.restore();
+      yield ;
+      [x0,y0]=[x1,y1];
+    }
+    c3.clearRect(-c3.canvas.width/2,-c3.canvas.height/2, c3.canvas.width,c3.canvas.height);
+    yield ;
+  }
+}
 
+function* animation_straight(){
+  c4.transform(1,0,0,-1,c4.canvas.width/2,c4.canvas.height/2);
+  c4.strokeStyle="SpringGreen";
+  c4.fillStyle="Green";
+  c4.lineWidth=3;
+  c4.font="1em sans-serif";
+  while(true){
+    let s=-180;
+    for(let t=s;t<=180;t++){
+      c4.clearRect(-c4.canvas.width/2,-c4.canvas.height/2, c4.canvas.width,c4.canvas.height);
+      c4.beginPath();
+      c4.moveTo(2*s,s+1);
+      c4.lineTo(2*t,t+1);
+      c4.stroke();
+      c4.save();
+      c4.setTransform(1,0,0,1,c4.canvas.width/2,c4.canvas.height/2);
+      c4.fillText(`t=${t}`, -c2.measureText(`t=${t}`).width/2, 200);
+      c4.restore();
+      yield ;
+    }
+  }
 }
 function* animate_poly() {
   let x = new Array<number>(32);
--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,15 @@ hljs.registerLanguage('c',c);
 import AI from './components/AI.vue'
 import CLang from "./components/CLang.vue";
 import lottery from "./components/lottery.vue";
-import BasicElectrical from "./components/BasicElectrical.vue";
 import Home from "./components/Home.vue";
+import Electrical from "./components/Electrical.vue";
 
 const app=createApp(App);
 const routes = [
     {path: '/',component: Home},
     { path: '/clang', component: CLang, },
     { path: '/ai', component: AI, },
-    { path: '/electrical', component: BasicElectrical},
+    { path: '/electrical', component: Electrical},
     {path: '/lottery',component: lottery}
 ]
 
