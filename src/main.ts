2943e9614f879cec952e4595c7619b670bb585ef
Wei He <weihe86@outlook.com>
2023-04-10 12:28:55 +0800

    add homework 2 of electrical

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ import Residue from "./electrical/residue/Residue.vue";
 import Fundamental from "./clang/Fundamental.vue";
 import Loop from "./clang/loop/Loop.vue";
 import Transport from "./ai/transport/Transport.vue";
+import EHomework2 from "./electrical/EHomework2.vue";
 
 const app=createApp(App);
 const routes = [
@@ -29,7 +30,8 @@ const routes = [
     {path: '/ai/transport',component: Transport},
     {path: '/ai/smart-home', component: SmartHome},
     {path: '/electrical/series', component: Series},
-    {path: '/electrical/residue',component: Residue}
+    {path: '/electrical/residue',component: Residue},
+    {path:'/electrical/homework-2',component: EHomework2}
 ]
 
 const router = createRouter({
