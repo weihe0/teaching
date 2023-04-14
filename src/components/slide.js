cf271ef1953fb1c5befaa27811a5f1549da3d783
Wei He <weihe86@outlook.com>
2023-04-14 20:58:28 +0800

    add katex

--- src/components/slide.js
+++ src/components/slide.js
@@ -1,10 +1,19 @@
-export default function Slide(props){
-    return <section className="slide">
-        <div className="title">
-            <h1>{props.title}</h1>
-        </div>
-        <div className="body">
-            {props.children}
-        </div>
-    </section>
+import {useEffect, useRef} from "react";
+import renderMathInElement from "katex/contrib/auto-render";
+
+export default function Slide(props) {
+  const body = useRef(null)
+  useEffect(() => {
+    renderMathInElement(body.current, {
+      output: 'html',
+    })
+  })
+  return <section className="slide">
+    <div className="title">
+      <h1>{props.title}</h1>
+    </div>
+    <div className="body" ref={body}>
+      {props.children}
+    </div>
+  </section>
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-
+import 'katex/dist/katex.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
