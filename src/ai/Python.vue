d03f5526c31057ba76220334033f8c2989e2c70f
Wei He <weihe86@outlook.com>
2023-05-19 12:15:59 +0800

    Pytorch

--- src/ai/Python.vue
+++ src/ai/Python.vue
@@ -187,6 +187,95 @@ const slides: Component[] = [
         </Slide>
       )
     }
+  },
+  {
+    render(){
+      return <Slide title='Pytorch'>
+        <ul>
+          <li>Python的人工智能<em>开发框架</em></li>
+          <li>前身是纽约大学开发的Torch框架</li>
+          <li>Meta公司（原Facebook）基于Torch研发PyTorch</li>
+        </ul>
+      </Slide>
+    }
+  },
+  {
+    render(){
+      return <Slide title='PyTorch的特点'>
+        <ul>
+          <li>大公司的支持</li>
+          <li>灵活强大的接口</li>
+          <li>非常多的扩展</li>
+          <li>支持更多的云平台</li>
+          <li>丰富的教学资源</li>
+        </ul>
+      </Slide>
+    }
+  },
+  {
+    render(){
+      return <Slide title='PyTorch的数据类型'>
+        <ul>
+          <li>向量<code>[1,2]</code></li>
+          <li>矩阵<code>[[1,2],[3,4]]</code></li>
+          <li>张量<code>[[[1,2],[3,2]],[[1,7],[5,4]]]</code></li>
+        </ul>
+      </Slide>
+    }
+  },
+  {
+    render(){
+      return <Slide title='常见信息的数据类型'>
+        <ul>
+          <li>原始文本：向量</li>
+          <li>处理后的文本：矩阵，每个向量代表一个<em>单词</em></li>
+          <li>声音：矩阵，每个向量代表一个<em>声道</em></li>
+          <li>图像：3维张量</li>
+          <li>视频：4维张量</li>
+        </ul>
+      </Slide>
+    }
+  },
+  {
+    render(){
+      return <Slide title='神经网络'>
+        <ul>
+          <li>神经网络是目前<em>最流行、最有效</em>的人工智能技术</li>
+          <li>神经网络是模拟大脑的数学函数</li>
+          <li>语音识别：神经网络（语音）=文本</li>
+          <li>人类识别：神经网络（人脸）=名字</li>
+          <li>智能对话：神经网络（问题）=回答</li>
+        </ul>
+      </Slide>
+    }
+  },
+  {
+    render(){
+      return <Slide title='PyTorch实现神经网络的步骤'>
+        <ol>
+          <li>准备样本库，例如标记的人脸库，语音及对应的文本，问题及回答等等</li>
+          <li>设定神经网络</li>
+          <li>对神经网络的准确度打分</li>
+          <li>调整神经网络</li>
+          <li>重复第3和4步，直到神经网络的准确度达标</li>
+        </ol>
+      </Slide>
+    }
+  },
+  {
+    render(){
+      return <Slide title='难点'>
+        <ul>
+          <li>构造适合的神经网络，既要避免<em>欠拟合</em>，又要避免<em>过拟合</em></li>
+          <li>利用<em>注意力机制</em>分清数据的主次</li>
+          <li>选择适合的神经网络的评价标准</li>
+          <li>选择适合的调整神经网络的方法</li>
+          <li>参考资料：<a href='https://www.bilibili.com/video/BV1bx411M7Zx'>
+            深度学习之神经网络的结构
+          </a> </li>
+        </ul>
+      </Slide>
+    }
   }
 ]
 </script>
